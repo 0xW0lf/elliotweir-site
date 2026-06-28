4d4585f4c3467cff910d0f92d5b734b41d5ec3f7
Elliot Weir <elliotmbweir@gmail.com>
2026-06-28 16:34:50 +0100

    Adding Site Map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  site: 'https://elliotweir.co.uk',
+  integrations: [sitemap()],
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
